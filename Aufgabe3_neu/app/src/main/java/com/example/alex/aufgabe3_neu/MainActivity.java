3409a78aa271ac6641e2413ea2bd67efa03af687
braunale <alexanderbraun95@aol.com>
2018-12-10 18:41:04 +0100

    Added "GetToken"-Button to the main activity. App crashes when clicked...

--- Aufgabe3_neu/app/src/main/java/com/example/alex/aufgabe3_neu/MainActivity.java
+++ Aufgabe3_neu/app/src/main/java/com/example/alex/aufgabe3_neu/MainActivity.java
@@ -3,32 +3,98 @@ package com.example.alex.aufgabe3_neu;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
 
-    String channelId  = "123456";
-    String channelName = "News";
+    private static final String TAG = "MainActivity";
 
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NotificationManager notificationManager =  getSystemService(NotificationManager.class);
+
+        Log.d(TAG,"\n\nTEST\n\n");
+
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            notificationManager.createNotificationChannel(
-                    new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_LOW));
+            // Create channel to show notifications.
+            String channelId  = "123456";
+            String channelName = "News";
+            NotificationManager notificationManager =
+                    getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(new NotificationChannel(channelId,
+                    channelName, NotificationManager.IMPORTANCE_LOW));
         }
 
+        // If a notification message is tapped, any data accompanying the notification
+        // message is available in the intent extras. In this sample the launcher
+        // intent is fired when the notification is tapped, so any accompanying data would
+        // be handled here. If you want a different intent fired, set the click_action
+        // field of the notification message to the desired intent. The launcher intent
+        // is used when no click_action is specified.
+        //
+        // Handle possible data accompanying notification message.
+        // [START handle_data_extras]
+        if (getIntent().getExtras() != null) {
+            for (String key : getIntent().getExtras().keySet()) {
+                Object value = getIntent().getExtras().get(key);
+                Log.d(TAG, "Key: " + key + " Value: " + value);
+            }
+        }
+        // [END handle_data_extras]
+
+
+        Button logTokenButton = findViewById(R.id.logTokenButton);
+        logTokenButton.setOnClickListener(new View.OnClickListener() {
 
 
 
+            @Override
+            public void onClick(View v) {
+
+                Log.d(TAG,"on click. Get Token!");
+
+                //Toast.makeText(MainActivity.this, "Test", Toast.LENGTH_SHORT).show();
+
+
+                // Get token
+                FirebaseInstanceId.getInstance().getInstanceId()
+                        .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                                if (!task.isSuccessful()) {
+                                    Log.w(TAG, "getInstanceId failed", task.getException());
+                                    return;
+                                }
+
+                                // Get new Instance ID token
+                                String token = task.getResult().getToken();
+
+                                // Log and toast
+
+                                Log.d(TAG, token);
+                                Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+                    
+            }
+        });
     }
 
 
